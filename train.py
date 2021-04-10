c53f47a5075a29abbb21dc4d258b0d5732363e38
Kieran Rigby <kieran.rigby@focusrite.com>
2021-04-10 03:12:12 +0100

    Added data preprocessing and starting to compile dataset

--- train.py
+++ train.py
@@ -22,6 +22,17 @@ def get_mfcc_of_chunks(chunks, sample_rate):
         mfcc.append(librosa.feature.mfcc(chunk, sample_rate, n_mfcc=12))
     return mfcc
 
+def balance_audio_length(audio_files):
+    minimum_duration = len(audio_files[0]["audio_data"])
+    for person in audio_files:
+        if len(person["audio_data"]) < minimum_duration:
+            minimum_duration = len(person["audio_data"])
+
+    for person in audio_files:
+        person["audio_data"] = person["audio_data"][0:minimum_duration]
+    
+    return audio_files
+
 def load_training_files(directory):
     files = directory.glob('*[a-zA-Z0-9].wav')
 
@@ -32,12 +43,27 @@ def load_training_files(directory):
     for file in files:
         print("- Loading " + file.name)
         audio, sr = librosa.load(file, sr=SAMPLE_RATE, mono=True)
-        audio_files.append({"name": file.name, "data": audio})
+        audio_files.append({"name": file.name, "audio_data": audio})
 
     return audio_files
 
-def preprocess(audio):
-    print(audio)
+def preprocess(audio_files):
+    print("Balancing duration of training samples...")
+    audio_files = balance_audio_length(audio_files)
+
+    print("Preprocessing Audio Files....")
+    for person in audio_files:
+        print(" - Preprocessing audio for " +  person["name"])
+        chunks = list(split_to_chunks(person["audio_data"], SAMPLE_RATE, CHUNK_DURATION_SECS))
+        mfccs = get_mfcc_of_chunks(chunks, SAMPLE_RATE)
+        person['chunks'] = chunks
+        person['mfccs'] = mfccs
+    
+    return audio_files
+
+def compile_dataset(preprocessed_audio):
+    print("Compiling dataset from audio samples...")
+    
 
 def main():
     input_directory = Path(args.input_dir)
@@ -47,6 +73,7 @@ def main():
 
         if (len(audio_files) > 0):
             preprocessed_audio = preprocess(audio_files)
+            x_train, x_test, y_train, y_test = compile_dataset(preprocessed_audio)
         else:
             print("Error: No valid audio files found in directory")
     else:
